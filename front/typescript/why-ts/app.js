660c67a1dcc3a92a81a34a2ad9ac1583c469d03a
youngman <sksn12@naver.com>
2022-01-29 16:36:26 +0900

    type

---
+++ front/typescript/why-ts/app.js
@@ -0,0 +1,48 @@
+// api url
+var url = "https://jsonplaceholder.typicode.com/users/1";
+
+// dom
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
+
+// user data
+var user = {};
+
+/**
+ * @typedef {object} Adress
+ * @property {string} street
+ * @property {string} city
+ */
+
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Adress} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+function fetchUser() {
+  return axios.get(url);
+}
+
+function startApp() {
+  fetchUser()
+    .then(function (response) {
+      console.log(response);
+      user = response.data;
+      // TODO: 이름, 이메일, 주소 표시하기
+
+      username.innerHTML = user.username;
+      email.innerHTML = user.email;
+      address.innerHTML = user.address;
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+startApp();
